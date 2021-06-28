4dbb13ee79acd8c27fac409020c42a42de3cbd24
SourceCode347 <80837445+sourcecode347@users.noreply.github.com>
2021-06-29 01:45:00 +0300

    Add files via upload

---
+++ mysql.php
@@ -0,0 +1,4 @@
+<?php
+	$con=mysqli_connect("localhost","root","ENTER MYSQL PASSWORD","ENTER DATABASE NAME");
+	if (mysqli_connect_errno()){echo "Failed to connect to MySQL: " . mysqli_connect_error();}
+?>
